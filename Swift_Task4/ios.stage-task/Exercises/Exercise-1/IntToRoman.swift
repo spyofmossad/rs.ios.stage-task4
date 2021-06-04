75658219c27dc4a042a89bb92abb1c68336630c8
spyofmossad <arbalestindigo@gmail.com>
2021-06-04 11:26:05 +0300

    feat: add exercise1 impl

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,24 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        var value = self
+        var output = ""
+        var romanMapping = [1:"I", 4:"IV", 5:"V", 9:"IX", 
+                            10:"X", 40:"XL", 50:"L", 90:"XC",
+                            100:"C", 400:"CD", 500:"D", 900:"CM", 1000:"M"]
+        var keys = Array(romanMapping.keys)
+        keys.sort {$0 > $1}
+        
+        for key in keys {
+            if key > value {
+                continue
+            }
+            while value >= key {
+                output += romanMapping[key]!
+                value = value - key
+            }
+        }
+        
+        return output
     }
 }
