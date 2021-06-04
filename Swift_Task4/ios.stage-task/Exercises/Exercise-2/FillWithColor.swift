0dea76e247027b5c107477f0a77a6f24e46a220d
spyofmossad <arbalestindigo@gmail.com>
2021-06-04 12:31:31 +0300

    feat: add exercise2 impl

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -1,8 +1,47 @@
 import Foundation
 
+struct Element {
+    var row: Int
+    var column: Int
+}
+
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        var outImage = image
+        var queue = [Element(row: row, column: column)]
+        let elementColor = image[row][column]
+        
+        while queue.count > 0 {
+            let element = queue.remove(at: 0)
+            
+            if (image[element.row][element.column] == elementColor) {
+                outImage[element.row][element.column] = newColor
+            } else {
+                continue
+            }
+            
+            if (element.row - 1) >= 0 {
+                queue.append(
+                    Element(row: element.row - 1, column: element.column)
+                )
+            }
+            if (element.row + 1) < image.count {
+                queue.append(
+                    Element(row: element.row + 1, column: element.column)
+                )
+            }
+            if (element.column - 1) >= 0 {
+                queue.append(
+                    Element(row: element.row, column: element.column - 1)
+                )
+            }
+            if (element.column + 1) < image[row].count {
+                queue.append(
+                    Element(row: element.row, column: element.column + 1)
+                )
+            }
+        }
+        return outImage
     }
 }
